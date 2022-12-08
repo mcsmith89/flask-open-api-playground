1e4c0d5b7b1a2aa9dd0c4d1d74f1029d8193b85c
Mike Smith <mcsmith1989@gmail.com>
2022-12-08 11:35:15 -0500

    Including updates for sqlite3

--- people.py
+++ people.py
@@ -1,49 +1,45 @@
 from datetime import datetime
 from flask import abort
+import sqlite3
 
+#SQLite connection
+def queryDB(query):
+    try:
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        return(cursor.execute(query))
+    except:
+        print(f"Error {query}")
+    
+
+#Get methods/endpoint functions
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
 
-people = {
-    "Hellbender": {
-        "fname": "James",
-        "lname": "Hellbender",
-        "timestamp": get_timestamp(),
-    },
-    "Fuckboi": {
-        "fname": "Billy",
-        "lname": "Fuckboi",
-        "timestamp": get_timestamp(),
-    },
-    "Dover": {
-        "fname": "Ben",
-        "lname": "Dover",
-        "timestamp": get_timestamp(),
-    }
-
-}
-
 def read_all():
-    return list(people.values())
+    players = {}
+    queryAllPLayers = f"SELECT * FROM players;"
+    for player in queryDB(queryAllPLayers).fetchall():
+        players.update({player[1]:{"fname":player[0], "lname":player[1], "created":player[2]}})
+    return(list(players.values()))
 
+
+#FIX THIS HERE 
 def create(person):
     lname = person.get("lname")
     fname = person.get("fname", "")
-
-    if lname not in people:
-        people.update({
-            lname:{
-                "fname":lname,
-                "lname":lname,
-                "timestamp":get_timestamp()
-            }
-        })
-
-        return people[lname], 201
-    
-    else:
+    createPlayer = f'''
+        INSERT INTO players (fname, lname, created)
+        VALUES
+        ("{lname}", "{fname}", DATE("now"));
+    '''
+    try:
+        queryDB(createPlayer).execute()
+        return(200)
+    except:
         abort(
             406,
-            f"Person with last name {lname} already exists",
+            f"Issue adding {fname} {lname}"
         )
+    
 #Left off on Create a New Person
